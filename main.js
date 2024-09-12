b214373900dd3960488a8d4b4cf29a88ec9330be
MegaTurtle <wsx86932321@yandex.ru>
2024-09-12 12:28:46 +0000

    ого чё то успел

--- main.js
+++ main.js
@@ -30,6 +30,7 @@ function setRegion(hotelName, region) {
     east
     */
     const data = JSON.parse(fs.readFileSync('./src/hotels.json', 'utf-8'))
+    if (!['central', 'north', 'south', 'west', 'east'].includes(region)) return 'Установлен неверный регион'
     data[hotelName] = regions[region]
     fs.writeFileSync('./src/hotels.json', JSON.stringify(data, null, 4), 'utf-8')
 }
@@ -38,9 +39,36 @@ function initHotel(name, region) {
     if (name.length > 150) return 'Название отеля не должно превышать 150 символов'
     let file = './src/hotels.json'
     const data = JSON.parse(fs.readFileSync(file, 'utf-8'))
-    data[name] = regions[region]
+    data[name] = {region: regions[region], rooms: [{id: 1, type: 'default', space: 2, status: 'free'}]}
     fs.writeFileSync(file, JSON.stringify(data, null, 4), 'utf-8')
 }
+
+/**
+  * Adds or rewrites hotel room
+  * @param {String} hotelName The name of the hotel
+  * @param {Number} id The id of the room in specified hotel
+  * @param {Literal} roomType The room type ([default, lux, apartments])
+  * @param {Literal} status Status of the room ([free, occupied])
+  */
+function manageHotelRoom(hotelName, id, roomType, status = 'free') {
+    let file = './src/hotels.json'
+    const data = JSON.parse(fs.readFileSync(file, 'utf-8'))
+    data[hotelName]["rooms"][id] = {id: id, type: roomType, space: `${roomType !== 'apartments' ? 2 : 4}`, status: status}
+    fs.writeFileSync(file, JSON.stringify(data, null, 4), 'utf-8')
+}
+
+function swapRoomStatus(hotelName, id) {
+    let file = './src/hotels.json'
+    const data = JSON.parse(fs.readFileSync(file, 'utf-8'))
+    data[hotelName]["rooms"][id].status = data[hotelName]["rooms"][id].status === 'occupied' ? 'free' : 'occupied'
+    fs.writeFileSync(file, JSON.stringify(data, null, 4), 'utf-8')
+}
+
+// function getAvailableRooms(hotelName) {
+//     let file = './src/hotels.json'
+//     const data = JSON.parse(fs.readFileSync(file, 'utf-8'))
+
+// }
 
 function print(...content) {
     return console.log(...content)

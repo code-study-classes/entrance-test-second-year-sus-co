22de3441b19ae77872012b7c77ce0bc6e302b502
MegaTurtle <wsx86932321@yandex.ru>
2024-09-13 09:01:24 +0000

    вау ещё чё то сделал

--- main.js
+++ main.js
@@ -21,25 +21,31 @@ function getRegion(hotelName) {
     return hotels[hotelName]
 }
 
-function setRegion(hotelName, region) {
-    /**
-    central
-    north
-    south
-    west
-    east
-    */
+function addRegion(name) {
+    const data = JSON.parse(fs.readFileSync('./src/regions.json', 'utf-8'))
+    data[name] = name
+    fs.writeFileSync('./src/regions.json', JSON.stringify(data, null, 4), 'utf-8')
+}
+
+function removeRegion(name) {
+    const data = JSON.parse(fs.readFileSync('./src/regions.json', 'utf-8'))
+    delete data[name]
+    fs.writeFileSync('./src/regions.json', JSON.stringify(data, null, 4), 'utf-8')
+}
+
+function setHotelRegion(hotelName, region) {
     const data = JSON.parse(fs.readFileSync('./src/hotels.json', 'utf-8'))
-    if (!['central', 'north', 'south', 'west', 'east'].includes(region)) return 'Установлен неверный регион'
+    if (!regions.includes(region)) return 'Установлен неверный регион'
     data[hotelName] = regions[region]
     fs.writeFileSync('./src/hotels.json', JSON.stringify(data, null, 4), 'utf-8')
 }
 
 function initHotel(name, region) {
     if (name.length > 150) return 'Название отеля не должно превышать 150 символов'
+    if (!regions.hasOwnProperty(region)) return 'Установлен неверный регион'
     let file = './src/hotels.json'
     const data = JSON.parse(fs.readFileSync(file, 'utf-8'))
-    data[name] = {region: regions[region], rooms: [{id: 1, type: 'default', space: 2, status: 'free'}]}
+    data[name] = {id: Math.floor(Math.random() * 100), region: regions[region], rooms: [{id: 1, type: 'default', space: 2, status: 'free'}]}
     fs.writeFileSync(file, JSON.stringify(data, null, 4), 'utf-8')
 }
 
@@ -53,22 +59,28 @@ function initHotel(name, region) {
 function manageHotelRoom(hotelName, id, roomType, status = 'free') {
     let file = './src/hotels.json'
     const data = JSON.parse(fs.readFileSync(file, 'utf-8'))
-    data[hotelName]["rooms"][id] = {id: id, type: roomType, space: `${roomType !== 'apartments' ? 2 : 4}`, status: status}
+    let roomIndex = data[hotelName]["rooms"].findIndex(room => room.id === id);
+    if (roomIndex !== -1) {
+        data[hotelName]["rooms"][roomIndex] = {id: id, type: roomType, space: `${roomType !== 'apartments' ? 2 : 4}`, status: status};
+    } else {
+        data[hotelName]["rooms"].push({id: id, type: roomType, space: `${roomType !== 'apartments' ? 2 : 4}`, status: status});
+    }
     fs.writeFileSync(file, JSON.stringify(data, null, 4), 'utf-8')
 }
+
+var mhr = manageHotelRoom
 
 function swapRoomStatus(hotelName, id) {
     let file = './src/hotels.json'
     const data = JSON.parse(fs.readFileSync(file, 'utf-8'))
-    data[hotelName]["rooms"][id].status = data[hotelName]["rooms"][id].status === 'occupied' ? 'free' : 'occupied'
+    data[hotelName]["rooms"][id - 1].status = data[hotelName]["rooms"][id - 1].status === 'occupied' ? 'free' : 'occupied'
     fs.writeFileSync(file, JSON.stringify(data, null, 4), 'utf-8')
 }
 
-// function getAvailableRooms(hotelName) {
-//     let file = './src/hotels.json'
-//     const data = JSON.parse(fs.readFileSync(file, 'utf-8'))
-
-// }
+function getAvailableRooms(hotelName) {
+    const freeRooms = hotels[hotelName].rooms.filter(room => room.status === "free")
+    return freeRooms.length > 0 ? freeRooms : 'В этом отеле нет свободных комнат'
+}
 
 function print(...content) {
     return console.log(...content)
